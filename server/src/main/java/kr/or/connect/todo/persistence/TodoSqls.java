124cb1b80cb76155b0e7ac8ad45946a077565945
yuns111 <sungk1003@naver.com>
2017-06-02 17:12:54 +0900

    selectAll
    완료 여부 구분 X

--- server/src/main/java/kr/or/connect/todo/persistence/TodoSqls.java
+++ server/src/main/java/kr/or/connect/todo/persistence/TodoSqls.java
@@ -4,4 +4,6 @@ public class TodoSqls {
 	static final String DELETE_BY_ID =
 			"DELETE FROM todo WHERE id= :id";
 	
+	static final String SELECT_ALL =
+			"SELECT id, todo, completed FROM todo";
 }
